160858be8a3570bb6eacd8d4be544d61e60ab17d
William Silva <euwilliamsantana@gmail.com>
2022-10-11 08:43:48 -0300

    Páginas e rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant={'success'} />
-      <Button variant={'danger'} />
-      <Button variant={'primary'} />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonColors, ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonColors
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
